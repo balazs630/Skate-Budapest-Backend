c5feaf29dcb59394df03dd6e13a963dfc0b8e5e4
Horváth Balázs <balazs630@icloud.com>
2018-11-26 21:58:43 +0100

    Update Package.swift dependency versions

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "SkateBudapestVapor",
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.1.0"),
     ],
     targets: [
         .target(name: "App", dependencies: ["Vapor"]),
