9497976031ae78abba6a726f18d87a52d55891fc
Horváth Balázs <balazs630@icloud.com>
2019-02-11 09:25:33 +0100

    Add SQLite and FluentSQLite dependencies

--- Package.swift
+++ Package.swift
@@ -5,9 +5,11 @@ let package = Package(
     name: "SkateBudapestVapor",
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.1.0"),
+        .package(url: "https://github.com/vapor/sqlite.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor"]),
+        .target(name: "App", dependencies: ["Vapor", "SQLite", "FluentSQLite"]),
         .target(name: "Run", dependencies: ["App"])
     ]
 )
