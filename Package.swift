1148ec46f5d54fe812cbb99e08f91f062ddc57a5
Horváth Balázs <balazs630@icloud.com>
2019-02-18 00:29:34 +0100

    Update Vapor package to latest

--- Package.swift
+++ Package.swift
@@ -4,12 +4,11 @@ import PackageDescription
 let package = Package(
     name: "SkateBudapestVapor",
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.1.0"),
-        .package(url: "https://github.com/vapor/sqlite.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.2.2"),
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "SQLite", "FluentSQLite"]),
+        .target(name: "App", dependencies: ["Vapor", "FluentSQLite"]),
         .target(name: "Run", dependencies: ["App"])
     ]
 )
