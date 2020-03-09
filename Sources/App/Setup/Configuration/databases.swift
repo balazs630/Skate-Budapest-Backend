d9526ad6688bd6fb3239b056f9e67f83719fe4e7
Horváth Balázs <balazs630@icloud.com>
2020-03-09 22:49:35 +0100

    Use EnvironmentVariable for psql connection configs

--- Sources/App/Setup/Configuration/databases.swift
+++ Sources/App/Setup/Configuration/databases.swift
@@ -10,7 +10,7 @@ import FluentPostgreSQL
 
 public func registerPostgeSQLDatabase(to services: inout Services, environment: Environment) throws {
     let databaseConnectionConfiguration = environment.isRelease
-        ? makeProductionPostgreSQLDatabaseConfig()
+        ? try makeProductionPostgreSQLDatabaseConfig()
         : makeDevelopmentPostgreSQLDatabaseConfig()
 
     let postgresDB = PostgreSQLDatabase(config: databaseConnectionConfiguration)
@@ -22,19 +22,19 @@ public func registerPostgreSQLProvider(to services: inout Services) throws {
 }
 
 private func makeDevelopmentPostgreSQLDatabaseConfig() -> PostgreSQLDatabaseConfig {
-    return PostgreSQLDatabaseConfig(hostname: "localhost",
-                                    port: 5432,
-                                    username: "user-1269852658",
-                                    database: "skate-budapest-1686949761",
-                                    password: nil,
+    return PostgreSQLDatabaseConfig(hostname: LocalConstant.psqlHostname,
+                                    port: LocalConstant.psqlPort,
+                                    username: LocalConstant.psqlUsername,
+                                    database: LocalConstant.psqlDatabase,
+                                    password: LocalConstant.psqlPassword,
                                     transport: .cleartext)
 }
 
-private func makeProductionPostgreSQLDatabaseConfig() -> PostgreSQLDatabaseConfig {
-    return PostgreSQLDatabaseConfig(hostname: Environment.get("SKTBPST_PSQL_IP")!,
-                                    port: Int(Environment.get("SKTBPST_PSQL_PORT")!)!,
-                                    username: Environment.get("SKTBPST_PSQL_USERNAME")!,
-                                    database: Environment.get("SKTBPST_PSQL_DATABASE")!,
-                                    password: Environment.get("SKTBPST_PSQL_PASSWORD")!,
+private func makeProductionPostgreSQLDatabaseConfig() throws -> PostgreSQLDatabaseConfig {
+    return PostgreSQLDatabaseConfig(hostname: try EnvironmentVariable.psqlIP.value(),
+                                    port: try EnvironmentVariable.psqlPort.value(),
+                                    username: try EnvironmentVariable.psqlUsername.value(),
+                                    database: try EnvironmentVariable.psqlDatabase.value(),
+                                    password: try EnvironmentVariable.psqlPassword.value(),
                                     transport: .unverifiedTLS)
 }
